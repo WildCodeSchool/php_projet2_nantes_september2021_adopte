f5d705926222867b757398b1f4cbe88f297cdcc8
Yann Malfer <yannmalfer@gmail.com>
2021-11-12 12:11:31 +0100

    Protection pages admin ok avec session

--- src/Controller/Private/AjoutChatController.php
+++ src/Controller/Private/AjoutChatController.php
@@ -5,6 +5,11 @@ namespace App\Controller\Private;
 use App\Model\Private\ChatManager;
 use App\Controller\AbstractController;
 
+session_start();
+if(!isset($_SESSION['login'])){
+    header ('location: /private/connexion');
+}       
+
 class AjoutChatController extends AbstractController
 {
     
