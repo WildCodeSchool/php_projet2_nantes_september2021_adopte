99636a95142b8a24e98a8c8e96a55fac0f92abee
CHARLES Nathalie <charlnathalie@gmail.com>
2021-11-13 20:08:05 +0100

    Formulaire ajout de Chat BDD

--- src/Controller/Private/ChatsController.php
+++ src/Controller/Private/ChatsController.php
@@ -7,30 +7,73 @@ use App\Controller\AbstractController;
 
 class ChatsController extends AbstractController{ 
 
-    public function add(): string
+    public $chat;
+    public $errors = [];
+    
+    public function verification(){
+        $this->chats = array_map('trim', $_POST);
+    }
+
+    public function uploadPhoto(){
+
+        //chemin vers le dossier sur le serveur qui reçois les photos
+        $uploadDir = "/assets/images/Cat";
+
+        //Recupère ext du fichier
+        $extension = pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
+
+        //Ext authorisées
+        $authorizedExtensions = ['jpg', 'jpeg', 'png'];
+
+        //Taille max de la photo
+        $maxFileSize = 2000000;
+
+        if( (!in_array($extension, $authorizedExtensions))){
+            $this->errors[] = 'Veuillez sélectionner un fichier au bon format (jpeg, jpg ou png)';
+
+        //Renomage du fichier
+        $uploadPhoto = $uploadDir . basename($_FILES['photo']['name']);
+
+        if (isset ($_FILES['photo']['tmp_name']) && filesize($_FILES['photo']['tmp_name']) > $maxFileSize)
+            $this->errors["photo"] = "L'image ne doit pas dépasser 2M";
+        } else {
+
+            //cryptage nom photo
+            $uploadPhoto = uniqid('photo', true) . '.' . $extension;
+             //chemin fichier pour la bdd
+            move_uploaded_file($_FILES['photo']['tmp_name'], $uploadPhoto);
+        }
+
+    }
+
+    public function add()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // clean $_POST data
-            $item = array_map('trim', $_POST);
-    
-            // TODO validations (length, format...)
-    
-            // if validation is ok, insert and redirection
-            
-            $chatManager = new ChatManager();
-            $id = $chatManager->insert($item);
-            header('Location:/items/show?id=' . $id);
-        }
-    
-        return $this->twig->render("Private/ajoutChat.html.twig");
-        }
-    public function edit(int $id)
-    {
-        $chatManager = new ChatManager();
-        $chat = $chatManager -> selectOneById($id);   
 
-        return $this->twig->render("Private/Chats/edit.html.twig", ['chat' => $chat]);
+            $this->verification();
+
+            if (empty($this->errors)){
+
+                $this->uploadPhoto();
+                $chatManager = new ChatManager();
+                $chatManager->insert($this->chat);
+                header('Location:/private/ajout');
+            }
+
+        return $this->twig->render("Private/ajoutChat.html.twig", ["errores" => $this->errors, 'action'=> "/private/add"]);
+
+        }
+
+        return $this->twig->render("Private/ajoutChat.html.twig");
     }
 
-    
 }
+
+//     public function edit(int $id)
+//     {
+//         $chatManager = new ChatManager();
+//         $chat = $chatManager -> selectOneById($id);   
+
+//         return $this->twig->render("Private/Chats/edit.html.twig", ['chat' => $chat]);
+//     }
--- src/Model/AdoptantManager.php
+++ src/Model/AdoptantManager.php
@@ -9,7 +9,7 @@ class AdoptantManager extends AbstractManager
      * Insert new item in database
      */
 
-    public function insertAdoptant(array $id): int
+    public function addAdoptant(array $id): int
     {
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`) VALUES (:name)");
         $statement->bindValue('name', $item['name'], \PDO::PARAM_STR);
