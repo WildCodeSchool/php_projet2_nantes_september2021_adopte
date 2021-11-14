991ad737de341bdb7166f82f23cf25be814a9b13
CHARLES Nathalie <charlnathalie@gmail.com>
2021-11-14 17:40:22 +0100

    conflits

--- src/Controller/Private/ChatsController.php
+++ src/Controller/Private/ChatsController.php
@@ -7,7 +7,6 @@ use App\Controller\AbstractController;
 
 class ChatsController extends AbstractController{ 
 
-<<<<<<< HEAD
     public $chat;
     public $errors = [];
     
@@ -18,32 +17,22 @@ class ChatsController extends AbstractController{
 
     public function uploadPhoto(){
 
-        //chemin vers le dossier sur le serveur qui reçois les photos
         $uploadDir = "public/assets/images/Cat";
-
-        //Recupère ext du fichier
         $extension = pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
-
-        //Ext authorisées
         $authorizedExtensions = ['jpg', 'jpeg', 'png'];
-
-        //Taille max de la photo
         $maxFileSize = 2000000;
 
         if( (!in_array($extension, $authorizedExtensions))){
             $this->errors[] = 'Veuillez sélectionner un fichier au bon format (jpeg, jpg ou png)';
 
-        //Renomage du fichier
         $uploadPhoto = $uploadDir . basename($_FILES['photo']['name']);
 
         if (isset ($_FILES['photo']['tmp_name']) && filesize($_FILES['photo']['tmp_name']) > $maxFileSize)
             $this->errors["photo"] = "L'image ne doit pas dépasser 2M";
         } else {
 
-            //cryptage nom photo
-            $uploadPhoto = uniqid('photo', true) . '.' . $extension;
-             //chemin fichier pour la bdd
-            move_uploaded_file($_FILES['photo']['tmp_name'], $uploadPhoto);
+        $uploadPhoto = uniqid('photo', true) . '.' . $extension;
+        move_uploaded_file($_FILES['photo']['tmp_name'], $uploadPhoto);
         }
 
     }
@@ -51,7 +40,6 @@ class ChatsController extends AbstractController{
     public function add()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
 
             $this->verification();
 
@@ -79,30 +67,12 @@ class ChatsController extends AbstractController{
 
 //         return $this->twig->render("Private/Chats/edit.html.twig", ['chat' => $chat]);
 //     }
-=======
+
     public function index()
     {
         $chatManager = new ChatManager();
         $chats = $chatManager->selectAll();
         return $this->twig->render("Private/chats.html.twig", ['chats' => $chats] );
-    }
-
-    public function add(): string
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
-            $item = array_map('trim', $_POST);
-    
-            // TODO validations (length, format...)
-    
-            // if validation is ok, insert and redirection
-            
-            $chatManager = new ChatManager();
-            $id = $chatManager->insert($item);
-            header('Location:/items/show?id=' . $id);
-        }
-    
-        return $this->twig->render("Private/ajoutChat.html.twig");
     }
 
     public function edit(int $id)
@@ -129,4 +99,3 @@ class ChatsController extends AbstractController{
         }
     }
 }
->>>>>>> 3f56914d2f167846dddca6f7b4b36f5a4149d58b
