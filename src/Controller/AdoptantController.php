e2e01011b2a3c08c96af1f193413dcb76eb4931f
d.chantreau <chantreaudamien@gmail.com>
2021-11-17 17:25:02 +0100

    Formulaire fonctionnel + message

--- src/Controller/AdoptantController.php
+++ src/Controller/AdoptantController.php
@@ -3,23 +3,40 @@
 namespace App\Controller;
 
 use App\Model\AdoptantManager;
-use App\Controller\AbstractController;
+use App\Controller\AbstractController;  
 
 class AdoptantController extends AbstractController 
-{
+{       
+    public $adoptant;
+    public $errors = [];
+    
+    // public function __construct()
+    //     {
+    //         parent::__construct();
+            
+    //     } 
+
+    public function verification(){
+        $this->adoptant = array_map('trim', $_POST);
+    }
 
 //Formulaire ajout d'adoptant
+
     public function addAdoptant()
         {   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+            $this->verification();
 
             if (empty($this->errors)){
 
                 $adoptantManager = new AdoptantManager();
                 $adoptantManager->addAdoptant($this->adoptant);
+                session_start();
+                $_SESSION['flashmessage']='merci pour l\'adoption.';
                 header('Location:/');
             }
 
-            return $this->twig->render("ficheChat.html.twig", ["errors" => $this->errors]);
+            return $this->twig->render("Home/listechats.html.twig", ["errors" => $this->errors]);
 
            }
 
@@ -66,4 +83,5 @@ class AdoptantController extends AbstractController
         }
             require __DIR__ . '/../views/charFormAdopt.html';
         }
+        
 }
