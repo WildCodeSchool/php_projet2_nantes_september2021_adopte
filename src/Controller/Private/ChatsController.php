ed1ef59e8b49d765d5e02cc14dbc3fbf8424936f
Sankatouille <affile.benjamin@gmail.com>
2021-11-18 13:58:37 +0100

    correction bug parti admin

--- src/Controller/Private/ChatsController.php
+++ src/Controller/Private/ChatsController.php
@@ -18,13 +18,6 @@ class ChatsController extends AbstractController
             header ('location: /private/connexion');
         }
 
-        $_SESSION = array();
-        if (isset($_COOKIE[session_name()]))
-        {
-            setcookie(session_name(),'',time()-4200, '/');
-        }
-
-        session_destroy();
 
     } 
 
